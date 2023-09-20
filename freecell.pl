5d78b9277c14b7835e63dba018eb546a8f708e6c
Raffael <mail@raffael-vogler.de>
2023-09-20 18:49:53 +0200

    rearrange predicates and special predicates for first goal. predicates moving something into the goal have to come first.
    predicates were missing for first addition to the goal.
    
    [trace]  ?- f([],[],[2,1],[3,4]).
       Call: (10) f([], [], [2, 1], [3, 4]) ? creep
       Call: (11) 2=:=3+1 ? creep
       Fail: (11) 2=:=3+1 ? creep
       Redo: (10) f([], [], [2, 1], [3, 4]) ? creep
       Call: (11) 3=:=2+1 ? creep
       Exit: (11) 3=:=2+1 ? creep
       Call: (11) f([], [], [1], [2, 3, 4]) ? creep
       Call: (12) f([1], [], [], [2, 3, 4]) ? creep
       Call: (13) 2=:=1+1 ? creep
       Exit: (13) 2=:=1+1 ? creep
       Call: (13) f([2, 1], [], [], [3, 4]) ? creep
       Call: (14) 3=:=2+1 ? creep
       Exit: (14) 3=:=2+1 ? creep
       Call: (14) f([3, 2, 1], [], [], [4]) ? creep
       Call: (15) 4=:=3+1 ? creep
       Exit: (15) 4=:=3+1 ? creep
       Call: (15) f([4, 3, 2, 1], [], [], []) ? creep
       Exit: (15) f([4, 3, 2, 1], [], [], []) ? creep
       Exit: (14) f([3, 2, 1], [], [], [4]) ? creep
       Exit: (13) f([2, 1], [], [], [3, 4]) ? creep
       Exit: (12) f([1], [], [], [2, 3, 4]) ? creep
       Exit: (11) f([], [], [1], [2, 3, 4]) ? creep
       Exit: (10) f([], [], [2, 1], [3, 4]) ? creep
    true .

--- freecell.pl
+++ freecell.pl
@@ -6,12 +6,20 @@
 % A for Lane A [a_1,...,a_k]
 % B for Lane B [b_1,...,b_k]
 
-% lanes to free cell
-f(G, [], [A|As], B) :- f(G, [A], As, B).
-f(G, [], A, [B|Bs]) :- f(G, [B], A, Bs).
+% done
+f(_,[],[],[]).
+
+% first goal
+f([], [1|Fs], A, B) :- f([1], Fs, A, B).
+f([], F, [1|As], B) :- f([1], F, As, B).
+f([], F, A, [1|Bs]) :- f([1], F, A, Bs).
 
 % free cell to goal
 f([G|Gs], [F], A, B) :- F =:= G + 1, f([F,G|Gs], [], A, B).
+
+% lanes to goal
+f([G|Gs], F, [A|As], B) :- A =:= G + 1, f([A,G|Gs], F, As, B).
+f([G|Gs], F, A, [B|Bs]) :- B =:= G + 1, f([B,G|Gs], F, A, Bs).
 
 % free cell to lanes
 f(G, [F], [A|As], B) :- F + 1 =:= A, f(G, [], [F,A|As], B).
@@ -21,9 +29,6 @@ f(G, [F], A, [B|Bs]) :- F + 1 =:= B, f(G, [], A, [F,B|Bs]).
 f(G, F, [A|As], [B|Bs]) :- A =:= B + 1, f(G, F, [B,A|As], Bs).
 f(G, F, [A|As], [B|Bs]) :- B =:= A + 1, f(G, F, As, [A,B|Bs]).
 
-% lanes to goal
-f([G|Gs], F, [A|As], B) :- A =:= G + 1, f([A,G|Gs], F, As, B).
-f([G|Gs], F, A, [B|Bs]) :- B =:= G + 1, f([B,G|Gs], F, A, Bs).
-
-% done
-f(_,[],[],[]).
+% lanes to free cell
+f(G, [], [A|As], B) :- f(G, [A], As, B).
+f(G, [], A, [B|Bs]) :- f(G, [B], A, Bs).
